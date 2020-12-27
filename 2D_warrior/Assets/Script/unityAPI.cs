442b003f923cf89d20a2287d23fabb025e596b12
Ameowmeow <47235810+Ameowmeow@users.noreply.github.com>
2020-12-27 16:36:41 +0800

    練習練習RRRRRRRR

---
+++ 2D_warrior/Assets/Script/unityAPI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// API
+/// </summary>
+public class unityAPI : MonoBehaviour
+{
+    public void Start()
+    {
+
+        #region 靜態屬性
+        print("隨機數字 : " + Random.value);
+
+        print("Pi : " + Mathf.PI);
+        #endregion
+
+        #region 靜態方法
+
+        int number = Random.Range(-10, 10);
+
+        print("隨機數字 : " + number);
+
+        print("取絕對值 : " + Mathf.Abs(number));
+
+        //練習
+
+        print("we have " + Camera.allCameras.Length + " camera.");
+
+
+
+
+        #endregion
+
+    }
+}
