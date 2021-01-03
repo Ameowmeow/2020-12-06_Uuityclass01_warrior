c31afb8a6001b47bbd846c659992f16379ab2c8c
Ameowmeow <47235810+Ameowmeow@users.noreply.github.com>
2021-01-03 13:00:46 +0800

    場景切換!!

---
+++ 2D_warrior/Assets/Script/API.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    public SpriteRenderer iteam01;
+
+    public GameObject iteamobject01;
+
+    public Transform iteamtransform01;
+
+    private void Start()
+    {
+        print("物件顏色:" + iteam01.color);
+
+        print("圖層:" + iteamobject01.layer);
+
+        iteam01.flipX = true;
+
+        print("翻轉左右" + iteam01.flipX);
+
+        iteamobject01.layer = 5;
+
+        print("改變圖層" + iteamobject01.layer);
+
+
+    }
+
+    public void Update()
+    {
+        iteamtransform01.Translate(0.5f, 0, 0, Space.World);
+    }
+}
---
+++ 2D_warrior/Assets/Script/ScenesCon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesCon : MonoBehaviour
+{
+    public void Startgame()
+    {
+        SceneManager.LoadScene("stage01");
+    }
+
+    public void Goback()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Endgame()
+    {
+        Application.Quit();
+    }
+}
