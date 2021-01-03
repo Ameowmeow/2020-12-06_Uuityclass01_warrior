c75a16399c448f8445ed4b9256207eda5573edc2
Ameowmeow <47235810+Ameowmeow@users.noreply.github.com>
2021-01-03 17:16:02 +0800

    人物移動

--- 2D_warrior/Assets/Script/ScenesCon.cs
+++ 2D_warrior/Assets/Script/ScenesCon.cs
@@ -3,17 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class ScenesCon : MonoBehaviour
 {
+    [Header("音效來源")]
+    public AudioSource music;
+
+    [Header("按鍵音效")]
+    public AudioClip sound;
+
     public void Startgame()
+    {
+        music.PlayOneShot(sound);
+        Invoke("delaystart", 1.0f);
+    }
+    public void delaystart()
     {
         SceneManager.LoadScene("stage01");
     }
 
     public void Goback()
     {
+        music.PlayOneShot(sound);
+        Invoke("delaygoback", 1.0f);
+    }
+    public void delaygoback()
+    {
         SceneManager.LoadScene("SampleScene");
     }
 
     public void Endgame()
+    {
+        music.PlayOneShot(sound);
+        Invoke("delayendgame", 1.0f);
+    }
+    public void delayendgame()
     {
         Application.Quit();
     }
